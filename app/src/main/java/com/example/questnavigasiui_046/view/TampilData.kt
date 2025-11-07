e1deadc14879bf84f794d35667691e1caab97f7e
anandajuliar <anandajulia001@gmail.com>
2025-11-07 16:41:19 +0700

    items to column

--- app/src/main/java/com/example/questnavigasiui_046/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_046/view/TampilData.kt
@@ -45,3 +45,11 @@ fun TampilData(
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.SpaceBetween){
             Column(modifier = Modifier.padding(all = dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy (space= dimensionResource(id = R.dimen.padding_small))){
+                items.forEach { items ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily= FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
